f7953254296f96cb34b13f7309e4f0dbd23bfe47
gabrielyoon7 <gabrielyoon7@gmail.com>
2023-03-14 01:08:54 +0900

    chore: eslint 규칙 설정

---
+++ .eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: ['airbnb'],
+  overrides: [
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  rules: {
+    'linebreak-style': 0,
+    'no-console': 0,
+    'import/extensions': [
+      'off',
+    ],
+  },
+};
